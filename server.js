af50560783a77a14c2461ef72e08b521f42c853c
SaraGhonim <saratarek2020@gmail.com>
2022-01-23 20:53:50 +0200

    chat-app

--- server.js
+++ server.js
@@ -7,16 +7,22 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+const users={}
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/client.html')
 });
 
+//every time user loads this website ==> it will call this function
+//it gives each user its own socket 
 io.on('connection', (socket) => {
     console.log('a new user connected');
-    socket.on('chat message', (msg) => {
+    socket.on("new user",userName=>{
+      users[socket.id]=userName
+    })
+    socket.on('chat message', (msg) => {   //it sends down to the client this msg 
         console.log('message: ' + msg);
-        io.emit('chat message', msg);
+         socket.broadcast.emit('chat message', {message:msg , name:users[socket.id]});//send msg down to the other clients and the sender's name
       });
   });
 
