4d3c43190d2e08e4e4e0d4cbbe05ad03ee14ceac
SaraGhonim <saratarek2020@gmail.com>
2022-01-24 15:12:52 +0200

    Caching Users Msgs using Redis

--- server.js
+++ server.js
@@ -7,8 +7,20 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+const redis = require("redis")
+const client=redis.createClient()
 const users={}
 
+var sendMessages=function(socket){
+  client.lrange("messages","0","-1", (err,data)=>{  //looping through each element in this messages array inside the redis server  
+  data.map(x=>{
+    const userMessage=x.split(":")  // for each string we sperate the message and the username 
+    const redisUserName=userMessage[0] //save username here
+    const redisMessage=userMessage[1]  //save message here
+    socket.emit('chat message',{message:redisMessage , name:redisUserName}) // send message to the client who joined or reloaded (triggred this event)
+  })
+  });
+}
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/client.html')
 });
@@ -17,11 +29,13 @@ app.get('/', (req, res) => {
 //it gives each user its own socket 
 io.on('connection', (socket) => {
     console.log('a new user connected');
+    sendMessages(socket)  //when a user join the channel bring all the old messages
     socket.on("new user",userName=>{
       users[socket.id]=userName
     })
     socket.on('chat message', (msg) => {   //it sends down to the client this msg 
         console.log('message: ' + msg);
+         client.rPush("messages",`${users[socket.id]} : ${msg}`) //push this user's msg to the messages array saved in the redis server
          socket.broadcast.emit('chat message', {message:msg , name:users[socket.id]});//send msg down to the other clients and the sender's name
       });
   });
